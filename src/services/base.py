c9fc1f3fa2bc93b58448b72980b23f778fad4654
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-08 23:09:26 +0300

    create base service

---
+++ src/services/base.py
@@ -0,0 +1,27 @@
+from abc import ABC, abstractmethod
+
+
+class BaseService(ABC):
+
+    def __init__(self, repository):
+        self.repository = repository
+
+    @abstractmethod
+    async def get_all(self):
+        return await self.repository.get_all()
+
+    @abstractmethod
+    async def get_one(self, id_):
+        return await self.repository.get_one(id_)
+
+    @abstractmethod
+    async def create_one(self, data):
+        return await self.repository.create_one(data)
+
+    @abstractmethod
+    async def update_one(self, id_, data):
+        return await self.repository.update_one(id_, data)
+
+    @abstractmethod
+    async def delete(self, id_):
+        return await self.repository.delete(id_)
