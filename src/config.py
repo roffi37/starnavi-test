85b4ae4f256ad50e220bf0268a36c6ae008496c5
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-12 22:36:51 +0300

    config file created

--- src/config.py
+++ src/config.py
@@ -1,17 +1,16 @@
-from functools import lru_cache
-
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class DatabaseSettings(BaseSettings):
     POSTGRES_HOST: str
-    POSTGRES_PORT: int
+    POSTGRES_PORT: str
     POSTGRES_USER: str
     POSTGRES_PASSWORD: str
     POSTGRES_DB: str
 
     def generate_async_database_url(self) -> str:
         url = (
+            "postgresql+asyncpg://"
             f"{self.POSTGRES_USER}:"
             f"{self.POSTGRES_PASSWORD}@"
             f"{self.POSTGRES_HOST}:"
@@ -20,14 +19,19 @@ class DatabaseSettings(BaseSettings):
         )
         return url
 
+    model_config = SettingsConfigDict(
+        env_file=".env",
+    )
+
+
+database_settings = DatabaseSettings()
+
 
 class Settings(BaseSettings):
-    database: DatabaseSettings
-
-    class Config:
-        env_file = ".env"
+    database: DatabaseSettings = database_settings
 
 
-@lru_cache
+
 def get_settings():
-    return Settings()
+    return Settings(database=database_settings)
+
