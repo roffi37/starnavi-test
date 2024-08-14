e1feb01823dd0a84e78eca02fa8e345ea1d8ae3a
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-15 01:40:25 +0300

    base repository updated

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,6 +1,15 @@
 from abc import ABC, abstractmethod
 
+from pydantic_settings import BaseSettings
+from sqlalchemy.ext.asyncio import AsyncSession
+
+
 class BaseRepository(ABC):
+    model = None
+
+    def __init__(self, session: AsyncSession, settings: BaseSettings):
+        self.session = session
+        self.settings = settings
 
     @abstractmethod
     async def get_all(self):
