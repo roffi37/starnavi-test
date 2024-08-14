05cc59c7d519c9a30b6a46937eca43f077774931
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-15 01:40:59 +0300

    user schemas updated

--- src/schemas/user.py
+++ src/schemas/user.py
@@ -1,10 +1,16 @@
 from datetime import datetime
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 
 
-class UserSchema(BaseModel):
-    uuid: str
+class UserCreateSchema(BaseModel):
     email: EmailStr
     username: str
     password: str
+
+
+class UserSchema(UserCreateSchema):
+    id: int
     created_at: datetime
+    updated_at: Optional[datetime]
