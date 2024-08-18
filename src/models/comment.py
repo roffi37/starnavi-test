2a01c8c3b63ac6bba63f88e5253ff0e7e00c6204
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-19 00:57:43 +0300

    update comment model

--- src/models/comment.py
+++ src/models/comment.py
@@ -1,7 +1,9 @@
+from datetime import date
+
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from src.schemas.comment import CommentSchema
+from src.schemas.comment import CommentSchema, DailyBreakdown
 from src.db.db import Base
 
 
@@ -12,7 +14,7 @@ class Comment(Base):
     likes: Mapped[int] = mapped_column(default=0)
     dislikes: Mapped[int] = mapped_column(default=0)
     author_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    is_blocked: Mapped[bool] = mapped_column(default=False)
+    blocked_at: Mapped[date] = mapped_column(nullable=True)
     author: Mapped["User"] = relationship(
         back_populates="comments",
     )
@@ -28,7 +30,7 @@ class Comment(Base):
             likes=self.likes,
             dislikes=self.dislikes,
             author_id=self.author_id,
-            is_blocked=self.is_blocked,
+            blocked_at=self.blocked_at,
             post_id=self.post_id,
             created_at=self.created_at,
             updated_at=self.updated_at,
