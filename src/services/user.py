60005d494e34bed02853908f483ce2b56dbd006c
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-21 03:22:46 +0300

    user services updated

--- src/services/user.py
+++ src/services/user.py
@@ -1,11 +1,29 @@
 from fastapi import Depends
 
+from src.exceptions import sign_in_error
 from src.services.base import BaseService
 from src.repositories.user import get_user_repository, UserRepository
+from src.utils.security import hash_password
+from src.utils.security import validate_password
+from src.utils.auth import create_access_token
 
 
 class UserService(BaseService):
-    pass
+
+    async def create_one(self, schema):
+        data = schema.model_dump()
+        password = data.pop("password")
+        data["password"] = hash_password(password)
+        return await self.repository.create_one(data)
+
+    async def sign_in(self, schema) -> dict:
+        data = schema.model_dump()
+        email = data.pop("email")
+        password = data.pop("password")
+        user_password = await self.repository.get_one_by_email(email)
+        if validate_password(password, user_password):
+            return create_access_token(email)
+        raise sign_in_error
 
 
 def get_user_service(repository: UserRepository = Depends(get_user_repository)):
