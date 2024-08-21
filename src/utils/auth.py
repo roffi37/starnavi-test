cfe372ea645fcc62210e70fc01f0088299c6bd13
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-22 00:45:30 +0300

    get_current_user added

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -1,7 +1,15 @@
 import jwt
 from datetime import datetime, UTC, timedelta
 
+from fastapi import Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from jwt import InvalidSignatureError
+
+from src.exceptions import invalid_signature_error
 from src.config import get_settings
+
+
+http_bearer = HTTPBearer()
 
 
 settings = get_settings()
@@ -32,9 +40,9 @@ def decode_jwt(
         token: str,
 ) -> dict:
     decoded = jwt.decode(
-        token=token,
+        jwt=token,
         key=settings.jwt_settings.JWT_SECRET_KEY,
-        algorithms=[settings.jwt_settings.JWT_ALGORITHM],
+        algorithms=[settings.jwt_settings.ALGORITHM],
     )
     return decoded
 
@@ -43,3 +51,14 @@ def create_access_token(email: str) -> dict:
     payload: dict = {"sub": email}
     token = encode_jwt(payload, 5)
     return {"access_token": token, "token_type": "Bearer"}
+
+
+def get_current_user(
+        token: HTTPAuthorizationCredentials = Depends(http_bearer)
+):
+    try:
+        payload = decode_jwt(token.credentials)
+        if payload.get("sub"):
+            return payload.get("sub")
+    except InvalidSignatureError:
+        raise invalid_signature_error
