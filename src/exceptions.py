e6bfa418b2fc34aa029447ffbfbdf9d303d41886
Serhii Martynenko <martinenko.sergiy@gmail.com>
2024-08-29 21:42:27 +0300

    added exception

--- src/exceptions.py
+++ src/exceptions.py
@@ -4,3 +4,5 @@ from fastapi import HTTPException
 sign_in_error = HTTPException(status_code=401, detail="Incorrect email or password")
 
 invalid_signature_error = HTTPException(status_code=401, detail="Invalid JWT token")
+
+relation_not_found = HTTPException(status_code=404, detail="Relation not found")
